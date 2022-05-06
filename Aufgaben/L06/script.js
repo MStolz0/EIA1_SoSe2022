aa0e3c92fa7dfa0f23267a2b70ec44214fa4b02a
MStolz0 <102662298+MStolz0@users.noreply.github.com>
2022-05-06 17:20:41 +0200

    Add script DE

--- Aufgaben/L06/script.js
+++ Aufgaben/L06/script.js
@@ -1,2 +1,8 @@
-console.log("hallo welt");
+var de_2022 = 83.75;
+var de_2008 = 82.11;
+var eu = 444.7;
+var de_relativ_eu = 100 / eu * de_2022;
+var wachstum = de_2022 / de_2008;
+var wachstum_gesamt = de_2022 - de_2008;
+console.log("Gesamteinwohnerzahl 2022 in Deutschland " + de_2022 + "Mio. Einwohnerzahl relativ zur Eu " + de_relativ_eu + "% Wachstumsrate seit 2008 " + wachstum + "% Wachstumsrate Gesamt zwischen 2008 und 2022 " + wachstum_gesamt + "Mio. ");
 //# sourceMappingURL=script.js.map
